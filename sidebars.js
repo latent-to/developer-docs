76c80922e18f5da625b782ef429c7e77f2969d75
michael trestman <m@latent.to>
2025-10-08 08:46:35 -0700

    wip

--- sidebars.js
+++ sidebars.js
@@ -203,6 +203,7 @@ const sidebars = {
         "keys/handle-seed-phrase",
         "keys/coldkey-hotkey-security",
         "keys/working-with-keys",
+        "concepts/address-poisoning-scams",
         "keys/multisig",
         "keys/schedule-coldkey-swap",
       ],
